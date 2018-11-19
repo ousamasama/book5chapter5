bcb2b64acc5b37af30db9b4ea8acf9a17bdc3594
Ousama Elayan <ousamasama@gmail.com>
2018-11-19 12:25:17 -0600

    first commit

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,122 @@
+import { Route } from 'react-router-dom'
+import React, { Component } from "react"
+import AnimalList from './animal/AnimalList'
+import LocationList from './location/LocationList'
+import EmployeeList from './employee/EmployeeList'
+import OwnerList from './owner/OwnerList'
+
+
+export default class ApplicationViews extends Component {
+    // employeesFromAPI = [
+    //     { id: 1, name: "Jessica Younker" },
+    //     { id: 2, name: "Jordan Nelson" },
+    //     { id: 3, name: "Zoe LeBlanc" },
+    //     { id: 4, name: "Blaise Roberts" }
+    // ]
+
+    // locationsFromAPI = [
+    //     { id: 1, name: "Nashville North", address: "500 Circle Way" },
+    //     { id: 2, name: "Nashville South", address: "10101 Binary Court" }
+    // ]
+
+    // animalsFromAPI = [
+    //     { id: 1, name: "Doodles" },
+    //     { id: 2, name: "Jack" },
+    //     { id: 3, name: "Angus" },
+    //     { id: 4, name: "Henley" },
+    //     { id: 5, name: "Derkins" },
+    //     { id: 6, name: "Checkers" }
+    // ]
+
+    // ownersFromAPI = [
+    //     {id: 1, name: "Ousama"},
+    //     {id: 2, name: "Zac"},
+    //     {id: 3, name: "Elyse"},
+    //     {id: 4, name: "Brendan"}
+    // ]
+
+    state = {
+        employees: [],
+        locations: [],
+        animals: [],
+        owners: []
+    }
+
+    componentDidMount() {
+        const newState = {}
+
+        fetch("http://localhost:5002/animals")
+            .then(r => r.json())
+            .then(animals => newState.animals = animals)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employees => newState.employees = employees)
+            .then(() => this.setState(newState))
+            .then(() => fetch("http://localhost:5002/owners")
+            .then(r => r.json()))
+            .then(owners => newState.owners = owners)
+            .then(() => this.setState(newState))
+            .then(() => fetch("http://localhost:5002/locations")
+            .then(r => r.json()))
+            .then(locations => newState.locations = locations)
+            .then(() => this.setState(newState))
+    }
+
+    deleteAnimal = id => {
+        return fetch(`http://localhost:5002/animals/${id}`, {
+            method: "DELETE"
+        })
+        .then(e => e.json())
+        .then(() => fetch(`http://localhost:5002/animals`))
+        .then(e => e.json())
+        .then(animals => this.setState({
+            animals: animals
+        })
+      )
+    }
+
+    fireEmployee = id => {
+        return fetch(`http://localhost:5002/employees/${id}`, {
+            method: "DELETE"
+        })
+        .then(e => e.json())
+        .then(() => fetch(`http://localhost:5002/employees`))
+        .then(e => e.json())
+        .then(employees => this.setState({
+            employees: employees
+        })
+      )
+    }
+
+    removeCustomer = id => {
+        return fetch(`http://localhost:5002/owners/${id}`, {
+            method: "DELETE"
+        })
+        .then(e => e.json())
+        .then(() => fetch(`http://localhost:5002/owners`))
+        .then(e => e.json())
+        .then(owners => this.setState({
+            owners: owners
+        })
+      )
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Route exact path="/" render={(props) => {
+                    return <LocationList locations={this.state.locations} />
+                }} />
+                <Route exact path="/animals" render={(props) => {
+                    return <AnimalList deleteAnimal={this.deleteAnimal} animals={this.state.animals} />
+                }} />
+                <Route path="/employees" render={(props) => {
+                    return <EmployeeList fireEmployee={this.fireEmployee} employees={this.state.employees} />
+                }} />
+                <Route path="/owners" render={(props) => {
+                    return <OwnerList removeCustomer={this.removeCustomer} owners={this.state.owners} />
+                }} />
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/components/animal/AnimalList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import dog from "./DogIcon.png"
+import "./animal.css"
+
+export default class AnimalList extends Component {
+    render () {
+        return (
+            <section className="animals list">
+            {
+                this.props.animals.map(animal =>
+                    <div key={animal.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                <img src={dog} className="icon--dog" />
+                                {animal.name}
+                                <a href="#"
+                                    onClick={() => this.props.deleteAnimal(animal.id)}
+                                    className="card-link">Delete</a>
+                            </h5>
+                        </div>
+                    </div>
+                )
+            }
+            </section>
+        )
+    }
+}
